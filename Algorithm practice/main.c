13f7c8f16d284525bd0014f0917ea4f481540fb5
常昊 <changhao@xiyou3g.com>
2018-04-13 17:31:23 +0800

    Initial Commit

---
+++ Algorithm practice/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Algorithm practice
+//
+//  Created by Howard on 2018/4/13.
+//  Copyright © 2018年 Howard. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
